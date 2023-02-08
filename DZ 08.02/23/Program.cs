3013fb6845731fa793f711d5f83199f00fd51e9e
Viktoria <android1986mir@rambler.ru>
2023-02-08 16:11:50 +0300

    Exersize23

--- DZ 08.02/23/Program.cs
+++ DZ 08.02/23/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+//3 -> 1, 4, 9. 
+//5 -> 1, 8, 27, 64, 125
+
+Console.Clear();
+Console.Write("Введите число N:");
+int N = int.Parse(Console.ReadLine());
+int i = 1;
+if (N<0){
+    i=N;
+    N=-1;
+}
+for(;i<=N;i++){
+Console.WriteLine($"{Math.Pow(i,3)}");
+}
