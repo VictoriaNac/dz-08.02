d1f842838d147b5ed01fc49345cb3947d494b704
Viktoria <android1986mir@rambler.ru>
2023-02-10 12:28:44 +0300

    DZ25

--- DZ10.02/25/Program.cs
+++ DZ10.02/25/Program.cs
@@ -2,3 +2,19 @@
 //3, 5 -> 243 (3⁵)
 //2, 4 -> 16
 
+int Exponentiation(int numberA, int numberB){
+  int result = 1;
+  for(int i=1; i <= numberB; i++){
+    result = result * numberA;
+  }
+    // int result = Math.Pow(numberA, numberB);
+    return result;
+}
+
+  Console.Write("Введите число A: ");
+  int numberA = Convert.ToInt32(Console.ReadLine());
+  Console.Write("Введите число B: ");
+  int numberB = Convert.ToInt32(Console.ReadLine());
+
+  int exponentiation = Exponentiation(numberA, numberB);
+  Console.WriteLine("Ответ: " + exponentiation);
